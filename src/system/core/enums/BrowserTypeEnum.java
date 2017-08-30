99e407c5e7326fd1f8629f0abf97141fb74b066b
yongqiangli <yongqiangli@aliyun.com>
2017-08-30 18:54:09 +0800

    1

--- src/system/core/enums/BrowserTypeEnum.java
+++ src/system/core/enums/BrowserTypeEnum.java
@@ -3,5 +3,5 @@ package system.core.enums;
  * @author yongqiangli
  */
 public enum BrowserTypeEnum {
-	IE6,IE7,IE8,IE9,IE10,IE11Firefox,Safari,Chrome,Opera,Camino,Gecko
+	IE6,IE7,IE8,IE9,IE10,IE11,Firefox,Safari,Chrome,Opera,Camino,Gecko,QQ,MAXTHON,GREEN,SE360
 }
